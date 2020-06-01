4e382906e4a1d5e0b0314845ccce7788140e6750
Henshaw, Andy <Andy.Henshaw@gtri.gatech.edu>
2020-06-01 10:31:15 -0400

    reset to before

--- hello/hello.py
+++ hello/hello.py
@@ -1,7 +1,6 @@
 import presenter
 import collaborator
 
-print(f"Hello from {presenter.first} {presenter.last}!!!")
-print(f"Hello from {collaborator.first} {collaborator.last}!")
 print(f"Hello from {presenter.first} {presenter.last}!")
+print(f"Hello from {collaborator.first} {collaborator.last}!")
 
